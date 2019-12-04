495064ddea82798eb5ee8a3e8f72da8640d0f7a3
devdigital <devdigital.cloud@gmail.com>
2019-12-04 11:24:16 +0000

    feat: implement deepmerge

--- packages/deepmerge/src/deepmerge.js
+++ packages/deepmerge/src/deepmerge.js
@@ -1,5 +1,24 @@
-const deepmerge = (obj1, obj2) => {
-  return obj1
+import { isObject } from '@utilz/types'
+import deepmergelib from 'deepmerge'
+
+const deepmerge = (obj1, obj2, defaultObj = {}) => {
+  if (!obj1 && !obj2) {
+    return defaultObj
+  }
+
+  if (isObject(obj1) && !obj2) {
+    return obj1
+  }
+
+  if (!obj1 && isObject(obj2)) {
+    return obj2
+  }
+
+  if (!isObject(obj1) || !isObject(obj2)) {
+    throw new Error('Must define at least one merge object.')
+  }
+
+  return deepmergelib(obj1, obj2)
 }
 
 export default deepmerge
--- packages/deepmerge/src/deepmerge.test.js
+++ packages/deepmerge/src/deepmerge.test.js
@@ -4,4 +4,54 @@ describe('deepmerge', () => {
   it('returns default empty object if inputs are undefined', () => {
     expect(deepmerge()).toEqual({})
   })
+
+  it('returns empty object if inputs are null', () => {
+    expect(deepmerge(null, null)).toEqual({})
+  })
+
+  it('returns default object if inputs are undefined', () => {
+    expect(deepmerge(undefined, undefined, { foo: 'bar' })).toEqual({
+      foo: 'bar',
+    })
+  })
+
+  it('returns default object if inputs are null', () => {
+    expect(deepmerge(null, null, { foo: 'bar' })).toEqual({ foo: 'bar' })
+  })
+
+  it('throws exception if input one is a string', () => {
+    expect(() => deepmerge('foo', null)).toThrow(
+      'Must define at least one merge object.'
+    )
+  })
+
+  it('throws exception if input two is a string', () => {
+    expect(() => deepmerge({}, 'foo')).toThrow(
+      'Must define at least one merge object.'
+    )
+  })
+
+  it('returns object one if input two is undefined', () => {
+    expect(deepmerge({ foo: 'bar' })).toEqual({ foo: 'bar' })
+  })
+
+  it('returns object one if input two is null', () => {
+    expect(deepmerge({ foo: 'bar' }, null)).toEqual({ foo: 'bar' })
+  })
+
+  it('returns object two if input one is undefined', () => {
+    expect(deepmerge(undefined, { foo: 'bar' })).toEqual({ foo: 'bar' })
+  })
+
+  it('returns object two if input one is null', () => {
+    expect(deepmerge(null, { foo: 'bar' })).toEqual({ foo: 'bar' })
+  })
+
+  it('does perform deep merge', () => {
+    expect(deepmerge({ foo: { bar: 'baz' } }, { foo: { baz: 'bar' } })).toEqual(
+      {
+        foo: { bar: 'baz', baz: 'bar' },
+      }
+    )
+  })
 })
