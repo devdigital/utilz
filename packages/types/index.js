2db4faee512bf581b6b53a0102c8f7deeef2bc18
devdigital <devdigital.cloud@gmail.com>
2019-12-05 13:33:45 +0000

    chore(types): remove index (#6)

--- packages/types/index.js
+++
@@ -1,5 +0,0 @@
-export { isArray } from './src/is-array'
-export { isBoolean } from './src/is-boolean'
-export { isFunction } from './src/is-function'
-export { isObject } from './src/is-object'
-export { isString } from './src/is-string'
