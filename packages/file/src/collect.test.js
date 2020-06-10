1a880e9cdcfb25be34383166aa396309bf805f11
devdigital <devdigital.cloud@gmail.com>
2020-06-10 21:12:51 +0100

    feat: add options to walk and collect

--- packages/file/src/collect.test.js
+++ packages/file/src/collect.test.js
@@ -5,7 +5,7 @@ import { ext } from './ext'
 // TODO: mock file system
 describe('collect', () => {
   it('should return expected files and folders', async () => {
-    const items = await collect()(path.resolve(__dirname, './test'))
+    const items = await collect(path.resolve(__dirname, './test'))
     expect(items.map(({ name }) => name)).toEqual([
       '1.js',
       '2.js',
@@ -19,7 +19,10 @@ describe('collect', () => {
   })
 
   it('should return files only', async () => {
-    const items = await collect(ext('.js'))(path.resolve(__dirname, './test'))
+    const items = await collect(path.resolve(__dirname, './test'), {
+      filter: ext('.js'),
+    })
+
     expect(items.map(({ name }) => name)).toEqual([
       '1.js',
       '2.js',
@@ -31,7 +34,10 @@ describe('collect', () => {
   })
 
   it('should return files only as array', async () => {
-    const items = await collect(ext(['.js']))(path.resolve(__dirname, './test'))
+    const items = await collect(path.resolve(__dirname, './test'), {
+      filter: ext(['.js']),
+    })
+
     expect(items.map(({ name }) => name)).toEqual([
       '1.js',
       '2.js',
@@ -42,10 +48,10 @@ describe('collect', () => {
     ])
   })
 
-  it('should return files based on custom predicate', async () => {
-    const items = await collect(({ name }) => name === '2.js')(
-      path.resolve(__dirname, './test')
-    )
+  it('should return files based on custom filter', async () => {
+    const items = await collect(path.resolve(__dirname, './test'), {
+      filter: ({ name }) => name === '2.js',
+    })
 
     expect(items.map(({ name }) => name)).toEqual(['2.js', '2.js', '2.js'])
   })
