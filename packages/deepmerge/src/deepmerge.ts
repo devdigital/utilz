7966bb4254f5edb04c59aa87b4577e165ae6651e
devdigital <devdigital.cloud@gmail.com>
2021-08-03 22:21:46 +0100

    fix: update deepmerge types (#42)

--- packages/deepmerge/src/deepmerge.ts
+++ packages/deepmerge/src/deepmerge.ts
@@ -1,6 +1,5 @@
 import { configure } from './configure'
 import { Nullish } from '@utilz/types'
 
-export const deepmerge = <T extends Record<string, unknown>>(
-  ...params: Nullish<Record<string, unknown>>[]
-): T => configure()(...params)
+export const deepmerge = <T extends object>(...params: Nullish<object>[]): T =>
+  configure()(...params)
