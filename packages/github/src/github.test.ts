95f0dd92e06dbac6290fe9b0fa26eac4c8ddabb0
devdigital <devdigital.cloud@gmail.com>
2022-03-30 11:22:22 +0100

    wip: add cheerio

--- packages/github/src/github.test.ts
+++
@@ -1,8 +0,0 @@
-import { gitHubCommits } from '.'
-
-describe('github', () => {
-  it('should foo', async () => {
-    const commits = await gitHubCommits('ianlovell')
-    console.log(commits)
-  })
-})
