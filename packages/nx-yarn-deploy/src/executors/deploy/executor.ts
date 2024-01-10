46e851072e8955ee290dd63db298b7f876c01429
devdigital <devdigital.cloud@gmail.com>
2024-01-10 13:04:20 +0000

    wip: update

--- packages/nx-yarn-deploy/src/executors/deploy/executor.ts
+++ packages/nx-yarn-deploy/src/executors/deploy/executor.ts
@@ -1,14 +1,47 @@
+import path from 'node:path';
 import { ExecutorContext } from '@nx/devkit';
 import { NxYarnDeployExecutorSchema } from './schema';
+
+function log(message: string, data?: unknown) {
+  console.log(
+    'Nx Yarn Deploy Executor:',
+    message,
+    data ? JSON.stringify(data, null, 2) : ''
+  );
+}
+
+function logError(message: string) {
+  console.error('Nx Yarn Deploy Executor:', message);
+}
 
 export default async function runExecutor(
   options: NxYarnDeployExecutorSchema,
   context: ExecutorContext
 ) {
-  console.log('Executor ran for Nx Yarn Deploy', options);
-  console.log('context', JSON.stringify(context, null, 2));
+  log('Received options', options);
 
-  return {
-    success: true,
-  };
+  const projectFolder = path.resolve(context.root, options.projectFolderPath);
+
+  log('Running yarn npm release', { projectFolder });
+
+  try {
+    const { execa } = await import('execa');
+
+    const { stdout } = execa('yarn', ['npm', 'release'], {
+      cwd: projectFolder,
+    });
+
+    log('Release', stdout);
+
+    return {
+      success: true,
+    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    logError(error.message);
+
+    return {
+      success: false,
+    };
+  }
 }
